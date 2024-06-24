beb996e29c7d79a6f797f114ce4e56537da69796
MonicaAlvesP <monica52782@gmail.com>
2024-06-24 16:31:15 -0300

    add frontend

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def sala(request):
+    return HttpResponse("Entre em nossa sala de aula!")
+
+def sala2(request):
+    return HttpResponse("Entre em nossa sala de aula 2!")
