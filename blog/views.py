8271d039fb6a6085e20f4a306113dae6c3d268a8
MonicaAlvesP <monica52782@gmail.com>
2024-06-24 17:06:42 -0300

    ...

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
 def sala(request):
-    return HttpResponse("Entre em nossa sala de aula!")
+    return render(request, 'blog/sala.html', {})
 
 def sala2(request):
     return HttpResponse("Entre em nossa sala de aula 2!")
